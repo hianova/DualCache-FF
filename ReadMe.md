d8a1253b1214b68db4437bc41a52f0f5176d9a7b
hianova <hianova@hianovas-MacBook-Air.local>
2026-02-22 16:50:36 +0800

    init revise

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,3 @@
-#DualCache-FF
+# DualCache-FF (Fast and Furious)
+## License 
+[PolyForm-Noncommercial-1.0.0](https://polyformproject.org/licenses/noncommercial/1.0.0/)
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
-use std::collections::HashMap;
+use std::mem::MaybeUninit;
 use std::thread;
+use std::{collections::HashMap, sync::Arc};
 
 use anyhow::Result;
+use arc_swap::ArcSwap;
 use crossbeam::channel::{self, Receiver, Sender};
 
-pub struct DualCacheFf<K, V> {
-    main: Paginated,
-    mirror: Paginated,
+pub struct DualCacheFF<K, V, T> {
+    main: Paginated<T>,
+    mirror: ArcSwap<Paginated<T>>,
     index: HashMap<K, usize>,
     ring_buffer: Vec<Node<K, V>>,
     ring_read: usize,
@@ -19,7 +21,7 @@ pub struct Config {
     capacity: usize,
 }
 
-impl<K, V> From<Config> for DualCacheFf<K, V> {
+impl<K, V, T> From<Config> for DualCacheFF<K, V, T> {
     fn from(config: Config) -> Self {
         let (tx, rx): (Sender<usize>, Receiver<usize>) = channel::bounded(1000);
         thread::spawn(|| {
@@ -29,9 +31,9 @@ impl<K, V> From<Config> for DualCacheFf<K, V> {
             main: Paginated {
                 pages: Vec::with_capacity(config.capacity),
             },
-            mirror: Paginated {
+            mirror: ArcSwap::from_pointee(Paginated {
                 pages: Vec::with_capacity(config.capacity),
-            },
+            }),
             index: HashMap::with_capacity(config.capacity),
             ring_buffer: Vec::with_capacity(config.capacity),
             ring_read: 0,
@@ -42,10 +44,10 @@ impl<K, V> From<Config> for DualCacheFf<K, V> {
         }
     }
 }
-impl<K, V> DualCacheFf<K, V> {
+impl<K, V, T> DualCacheFF<K, V, T> {
     pub fn new() -> Self {
         let config = Config { capacity: 10 ^ 7 };
-        DualCacheFf::from(config)
+        DualCacheFF::from(config)
     }
     pub fn add(&mut self) -> Result<()> {
         Ok(())
@@ -62,24 +64,50 @@ mod tests {
 
     #[test]
     fn add() {
-        let result = DualCacheFf::<usize, usize>::new().add();
+        let result = DualCacheFF::<usize, usize, usize>::new().add();
         assert!(result.is_ok());
     }
     #[test]
     fn get() {
-        let result = DualCacheFf::<usize, usize>::new().get();
+        let result = DualCacheFF::<usize, usize, usize>::new().get();
         assert!(result.is_ok());
     }
 }
 
+const PAGE_SIZE: usize = 1024;
+const SHIFT: usize = 10;
+const MASK: usize = 1023;
+
 #[derive(Clone, Debug)]
 pub struct Node<K, V> {
     pub key: K,
-    pub value: V,
+    pub value: Arc<V>,
     pub counter: u64,
     pub time_stamp: u64,
 }
-struct Paginated {
-    pages: Vec<Page>,
+struct Paginated<T> {
+    pages: Vec<Arc<Page<T>>>,
 }
-struct Page {}
+impl<T: Clone> Clone for Paginated<T> {
+    fn clone(&self) -> Self {
+        Self {
+            pages: self.pages.clone(),
+        }
+    }
+}
+struct Page<T> {
+    data: [T; PAGE_SIZE],
+}
+impl<T: Clone> Clone for Page<T> {
+    fn clone(&self) -> Self {
+        let mut new_page: MaybeUninit<Page<T>> = MaybeUninit::uninit();
+        unsafe {
+            std::ptr::copy_nonoverlapping(
+                &self.data as *const T,
+                new_page.as_mut_ptr() as *mut T,
+                PAGE_SIZE,
+            );
+            new_page.assume_init()
+        }
+    }
+}
