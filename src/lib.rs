caefe49804f94f85ba9a953390fa8079ed96c0cf
hianova <ethan90827@gmail.com>
2026-04-01 07:45:01 +0800

    init revise

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,25 @@
 use ahash::AHashMap;
 use arc_swap::ArcSwap;
 use crossbeam::channel::{Receiver, Sender};
-use crossbeam::epoch::Atomic;
 use std::borrow::Borrow;
-use std::hash::Hash;
 use std::sync::Arc;
-use std::sync::atomic::AtomicI32;
+use std::sync::atomic::AtomicU32;
 
-const PAGE_SIZE: usize = 1024;
-const SHIFT: usize = 10;
-const MASK: usize = 1023;
+const PAGE_SIZE: usize = 128;
+const SHIFT: usize = 7;
+const MASK: usize = 127;
 
 /// # SPEC: Cache config for `DualCacheFF` & `Daemon`
 pub struct Config {
-    pub capacity: usize,
-    pub duration: u64,
+    pub capacity: usize, // will align 128 multipul at start
+    pub duration: u32,
 }
 /// # SPEC: Cache mirror for `.get` & modify by push `Action` to channel
 #[derive(Clone)]
 pub struct DualCacheFF<K, V> {
     view: Arc<ArcSwap<Cache<K, V>>>,
     action_tx: Sender<Action<K, V>>,
-    epoch: Arc<AtomicI32>,
+    epoch: Arc<AtomicU32>,
 }
 impl<K, V> DualCacheFF<K, V> {
     pub fn new(config: Config) -> Self {
@@ -29,21 +27,13 @@ impl<K, V> DualCacheFF<K, V> {
             "Cache::new(config.capacity) channel::bound(config.capacity) thread::spawn(Daemon.start(config))"
         )
     }
-    pub fn get<Q>(&self, key: &Q) -> Option<V>
-    where
-        K: Borrow<Q>,
-        Q: std::hash::Hash + Eq + ?Sized + std::fmt::Debug,
-    {
+    pub fn get(&self, index: usize) -> Option<V> {
         todo!("self.view.get(key) self.action_tx.try_send(Action::Get(key))")
     }
     pub fn insert(&self, key: K, value: V) {
         todo!("self.action_tx.try_send(Action::Insert(key, value))")
     }
-    pub fn remove<Q>(&self, key: &Q)
-    where
-        K: Borrow<Q>,
-        Q: std::hash::Hash + Eq + ?Sized + std::fmt::Debug,
-    {
+    pub fn remove(&self, index: usize) {
         todo!("self.action_tx.try_send(Action::Remove(key))")
     }
     pub fn clear() {
@@ -54,7 +44,7 @@ impl<K, V> DualCacheFF<K, V> {
 struct Daemon<K, V> {
     view: Arc<ArcSwap<Cache<K, V>>>,
     action_rx: Receiver<Action<K, V>>,
-    epoch: Arc<AtomicI32>,
+    epoch: Arc<AtomicU32>,
 }
 impl<K, V> Daemon<K, V> {
     fn start(&self, config: Config) {
@@ -69,44 +59,50 @@ impl<K, V> Daemon<K, V> {
 }
 /// # SPEC: Search K in index for `nodes` paginated index than high-low bitwise for V
 struct Cache<K, V> {
-    index: Arc<AHashMap<K, usize>>,
-    nodes: Arc<Vec<Arc<Page<K, V>>>>,
+    index: AHashMap<K, usize>,
+    pages: Vec<Page<K, V>>,
 }
 impl<K, V> Cache<K, V> {
-    fn get<Q>(&self, key: &Q) -> Option<V>
-    where
-        K: Borrow<Q>,
-        Q: std::hash::Hash + Eq + ?Sized + std::fmt::Debug,
-    {
+    fn get(&self, index: usize) -> Option<V> {
         todo!()
     }
-    fn put(&self, key: K, value: V) -> usize {
+    fn put(&self, key: K, value: V, target: usize) -> usize {
+        todo!()
+    }
+    fn remove(&self, index: usize) {
+        todo!()
+    }
+    fn clear(&self) {
         todo!()
     }
 }
 /// # SPEC: Arena feature count avg and epoch evict standard
 struct Arena {
-    records: Vec<usize>,
+    ranks: Vec<Record>,
     rev_map: Vec<usize>,
-    counts: Vec<u32>,
+    count_sum: u32,
     evict_point: usize,
     direction: isize,
-    count_sum: u32,
-    epoch: Arc<AtomicI32>,
 }
 impl Arena {
-    fn evict_next() -> usize {
+    fn get(&self) -> Option<usize> {
         todo!()
     }
-    fn hit_swap() {
+    fn put(&self) -> usize {
+        todo!()
+    }
+    fn remove(&self) {
+        todo!()
+    }
+    fn clear(&self) {
         todo!()
     }
 }
 /// # SPEC: Channel action
 enum Action<K, V> {
-    Get(K),
+    Get(usize),
     Insert(K, V),
-    Remove(K),
+    Remove(usize),
     Clear,
 }
 /// # SPEC: Node structure
@@ -114,9 +110,15 @@ enum Action<K, V> {
 struct Node<K, V> {
     key: K,
     value: V,
-    epoch: Arc<AtomicI32>,
+    epoch: u32,
 }
 /// # SPEC: Page structure
 struct Page<K, V> {
     nodes: Vec<Node<K, V>>,
+}
+/// # SPEC: Record structure
+struct Record {
+    index: usize,
+    count: u8,
+    epoch: u32,
 }
